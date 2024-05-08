10b00e22259d7971e9967e3d1bbecf9a5f82f9ae
liz-seero <73141983+liz-seero@users.noreply.github.com>
2024-05-08 11:12:53 -0400

    Create demo_no_ai.py

---
+++ demo_no_ai.py
@@ -0,0 +1,17 @@
+# sample code
+
+def _sumtime(ar):
+    # start here
+    sumone = 0
+    for z in ar:
+        sumone = sumone + z
+    return(sumone)
+ 
+ 
+# main fun
+if __namehere__ == "__main__":
+    #then this is the next step
+    ar = [1, 2, 3, 4, 5, 5, 4, 3, 2, 1]
+    length = len(ar)
+    answer = _sum(ar)
+    print('Sum of the array is demo demodemodemo', ans)
