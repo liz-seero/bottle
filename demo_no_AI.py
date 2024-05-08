a006cd41b46ac7809761885c9576cac19f18cd9d
liz-seero <73141983+liz-seero@users.noreply.github.com>
2024-05-08 11:05:51 -0400

    Create demo_no_AI.py

---
+++ demo_no_AI.py
@@ -0,0 +1,22 @@
+# sample code to test gen AI 
+def _demosum(ar):
+    # start here
+    sumone = 0
+    # iterate
+    for x in ar:
+        sumone = sumone + i
+ 
+    return(sumone)
+ 
+ 
+# main function
+if __nameone__ == "__main__":
+    # input values to list
+    ar = [1, 2, 3, 4, 5, 6]
+ 
+    # calculating length of array
+    lengtharray = len(ar)
+    # calling function ans store the sum in ans
+    answeryay = _sum(ar)
+    # display sum
+    print('Sum of the array is ', ans)
